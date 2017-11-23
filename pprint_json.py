41dfc7d0ca036512f1251c6a80474b45b7b18dcf
hramascuma <hramascuma@gmail.com>
2017-11-23 19:43:06 +0300

    template is full

--- pprint_json.py
+++ pprint_json.py
@@ -4,10 +4,10 @@ import sys
 def load_data(filepath):
     return json.load(open(filepath))
 
-def pretty_print_json(data):
-    print(json.dumps(data, sort_keys=True, indent=4))
+def pretty_print_json(json_content):
+    print(json.dumps(json_content, sort_keys=True, indent=4))
 
 if __name__ == '__main__':
     namespace = sys.argv[1]
-    data = load_data(namespace)
-    pretty_print_json(data)
+    json_content = load_data(namespace)
+    pretty_print_json(json_content)
